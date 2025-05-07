07ebcc796e7d977be111897ff08ccff7999b34a4
한지수 <eldeoddt@gmail.com>
2025-05-07 17:06:06 +0900

    [FEAT] 로그인 후 accessToken 로컬 스토리지에 저장 -> todo로 리다이렉트 추가

--- src/Login.js
+++ src/Login.js
@@ -1,7 +1,68 @@
 import React from "react";
+import { Container, Grid, Typography, TextField, Button } from "@mui/material";
+import { signin } from "./service/ApiService";
 
 const Login = () => {
-  return <p>로그인 페이지</p>;
+  const handleSubmit = (event) => {
+    // default submit의 동작을 막는다. 페이지 새로고침 방지.
+    event.preventDefault();
+
+    // 이벤트가 발생한 form의 모든 입력값을 수집한다.
+    // 해당 form 안의 input 값을 key(name)-value 객체로 변환한다.
+    const data = new FormData(event.target);
+    // username 입력값을 추출한다.
+    const username = data.get("username");
+    const password = data.get("password");
+
+    // apiservice의 signin 메서드를 사용하여 로그인한다.
+    signin({ username: username, password: password });
+  };
+
+  return (
+    <Container component="main" maxWidth="xs" style={{ marginTop: "8%" }}>
+      <Grid container spacing={2}>
+        <Grid item xs={12}>
+          <Typography component="h1" variant="h5">
+            로그인
+          </Typography>
+        </Grid>
+      </Grid>
+      <form noValidate onSubmit={handleSubmit}>
+        {" "}
+        {/* subimt 버튼 누르면 handleSubmit이 실행된다. */}
+        <Grid container spacing={2}>
+          <Grid item xs={12}>
+            <TextField
+              variant="outlined"
+              required
+              fullWidth
+              id="username"
+              label="아이디"
+              name="username"
+              autoComplete="username"
+            />
+          </Grid>
+          <Grid item xs={12}>
+            <TextField
+              variant="outlined"
+              required
+              fullWidth
+              name="password"
+              label="패스워드"
+              type="password"
+              id="password"
+              autoComplete="current-password"
+            />
+          </Grid>
+          <Grid item xs={12}>
+            <Button type="submit" fullWidth variant="contained" color="primary">
+              로그인
+            </Button>
+          </Grid>
+        </Grid>
+      </form>
+    </Container>
+  );
 };
 
 export default Login;
--- src/service/ApiService.js
+++ src/service/ApiService.js
@@ -1,10 +1,18 @@
 import { API_BASE_URL } from "../app-config";
 
 export function call(api, method, request) {
+  let headers = new Headers({
+    "Content-Type": "application/json",
+  });
+
+  // 로컬 스토리지에서 Access token 가져오기
+  const accessToken = localStorage.getItem("ACCESS_TOKEN");
+  if (accessToken && accessToken !== null) {
+    // 헤더에 토큰을 추가한다.
+    headers.append("Authorization", "Bearer " + accessToken);
+  }
   let options = {
-    headers: new Headers({
-      "Content-Type": "application/json",
-    }),
+    headers: headers,
     url: API_BASE_URL + api,
     method: method,
   };
@@ -34,8 +42,14 @@ export function call(api, method, request) {
 // 로그인 요청 함수
 export function signin(userDTO) {
   return call("/auth/signin", "POST", userDTO)
-    .then((response) => {
-    console.log("response: ", response);
-    alert("로그인 토큰: " + response.token);
+  .then((response) => {
+    // console.log("response: ", response);
+    // alert("로그인 토큰: " + response.token);
+    if (response.token) {
+      // 로컬 스토리지에 토큰 저장
+      localStorage.setItem("ACCESS_TOKEN", response.token);
+      // token 존재하는 경우 todo 화면으로 리다이렉트한다.
+      window.location.href = "/";
+    }
   });
 }
